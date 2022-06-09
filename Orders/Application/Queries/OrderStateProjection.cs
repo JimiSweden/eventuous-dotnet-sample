0dd114b3cc5755d72fc6fce25a75103b008a0f4b
Jimi Friis <jimi.friis@gmail.com>
2022-06-09 11:01:53 +0200

    wip add Order Projections order cancelled, order row added,deleted

--- Orders/Application/Queries/OrderStateProjection.cs
+++ Orders/Application/Queries/OrderStateProjection.cs
@@ -17,9 +17,13 @@ namespace Orders.Application.Queries
 
             On<OrderEvents.V1.OrderAdded>(evt => evt.OrderId, HandleOrderAdded);
             On<OrderEvents.V1.OrderBooked>(evt => evt.OrderId, HandleOrderBooked);
-
-
+            On<OrderEvents.V1.OrderCancelled>(evt => evt.OrderId, HandleOrderCancelled);
+            On<OrderEvents.V1.OrderCancelled>(evt => evt.OrderId, HandleOrderCancelled);
+            On<OrderEvents.V1.OrderRowAdded>(evt => evt.OrderId, HandleOrderRowAdded);
+            On<OrderEvents.V1.OrderRowDeleted>(evt => evt.OrderId, HandleOrderRowDeleted);
+            
             //TODO: order-rows : Add/remove/update
+
 
             On<OrderEvents.V1.PaymentRecorded>(
                 evt => evt.OrderId,
@@ -32,7 +36,23 @@ namespace Orders.Application.Queries
             );
         }
 
+        private UpdateDefinition<OrderDocument> HandleOrderRowDeleted(OrderEvents.V1.OrderRowDeleted evt, UpdateDefinitionBuilder<OrderDocument> update)
+            => update.PullFilter(x => x.OrderRows, row => row.OrderRowId == evt.OrderRowId);
         
+        private UpdateDefinition<OrderDocument> HandleOrderRowAdded(OrderEvents.V1.OrderRowAdded evt, UpdateDefinitionBuilder<OrderDocument> update)
+        
+        => update.AddToSet(x => x.OrderRows, new OrderDocument.OrderRow(
+            evt.OrderRowId, evt.ProductId, evt.ProductName,evt.ProductType,
+            evt.ProductAmount));
+        
+
+        private UpdateDefinition<OrderDocument> HandleOrderCancelled(OrderEvents.V1.OrderCancelled evt,
+            UpdateDefinitionBuilder<OrderDocument> update)
+            => update.Set(x => x.Cancelled, true)
+                .Set(x => x.CancelledBy, evt.CancelledBy)
+                .Set(x => x.CancelledReason, evt.Reason)
+                .Set(x => x.OrderCancelledDate, evt.OrderCancelledAt);
+
 
         private UpdateDefinition<OrderDocument> HandleOrderAdded(OrderEvents.V1.OrderAdded evt, UpdateDefinitionBuilder<OrderDocument> update)
             => update.SetOnInsert(x => x.Id, evt.OrderId)
@@ -79,5 +99,12 @@ namespace Orders.Application.Queries
         public DateTimeOffset OrderCancelledDate { get; set; }
         public string CancelledBy { get; set; }
         public string CancelledReason { get; set; }
+
+        public List<OrderRow> OrderRows { get; set; } = new ();
+
+        public record OrderRow (string OrderRowId, string ProductId, 
+            string ProductName, 
+            string ProductType,
+            int ProductAmount);
     }
 }
